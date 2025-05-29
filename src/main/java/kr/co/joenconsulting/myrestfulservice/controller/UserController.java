600f93141905163ad162ecb3760e2c0c13805e9b
gaeul <e0540@hanmail.net>
2025-05-30 00:52:08 +0900

    사용자 목록조회, 등록 구현, HTTP Status Code 제어

---
+++ src/main/java/kr/co/joenconsulting/myrestfulservice/controller/UserController.java
@@ -0,0 +1,46 @@
+package kr.co.joenconsulting.myrestfulservice.controller;
+
+import kr.co.joenconsulting.myrestfulservice.bean.User;
+import kr.co.joenconsulting.myrestfulservice.dao.UserDaoService;
+import kr.co.joenconsulting.myrestfulservice.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        User user = service.findOne(id);
+        if (user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
